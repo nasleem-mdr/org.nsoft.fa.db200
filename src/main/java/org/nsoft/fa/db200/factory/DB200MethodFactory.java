759d53237dba7651a26495330787c845e52db492
Nasleem <54586830+nasleem-mdr@users.noreply.github.com>
2025-12-24 13:29:31 +0700

    Add files via upload

---
+++ src/main/java/org/nsoft/fa/db200/factory/DB200MethodFactory.java
@@ -0,0 +1,22 @@
+package org.nsoft.fa.db200.factory;
+
+import org.idempiere.fa.service.api.DepreciationFactoryLookupDTO;
+import org.idempiere.fa.service.api.IDepreciationMethod;
+import org.idempiere.fa.service.api.IDepreciationMethodFactory;
+import org.nsoft.fa.db200.method.DDBScheme200;
+
+public class DB200MethodFactory implements IDepreciationMethodFactory {
+
+    // Konstanta untuk nama metode yang akan digunakan di database/Application Dictionary
+    public static final String DEPRECIATION_TYPE_DDB200 = "DB200";
+
+    @Override
+    public IDepreciationMethod getDepreciationMethod(DepreciationFactoryLookupDTO lookupDto) {
+    	
+    	if (DEPRECIATION_TYPE_DDB200.equals(lookupDto.depreciationType)) {
+            // Jika sistem meminta DDB_200, kembalikan instance kelas perhitungan kita
+            return new DDBScheme200();
+        }
+        return null; // Jika bukan, kembalikan null
+    }
+}
